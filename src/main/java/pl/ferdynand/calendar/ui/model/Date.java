14a1a43e83b42d331165212169a37046adf4cabb
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-11-15 01:48:28 +0100

    Add new Event and Date class

---
+++ src/main/java/pl/ferdynand/calendar/ui/model/Date.java
@@ -0,0 +1,49 @@
+package pl.ferdynand.calendar.ui.model;
+
+import pl.ferdynand.calendar.exceptions.DateFormatException;
+
+public class Date {
+    private int year;
+    private int month;
+    private int day;
+
+    public Date(String dayMonthYear){
+        String[] numbers = dayMonthYear.split(".");
+        if(numbers.length != 3)
+            throw new DateFormatException("Wrong date format, should be day.month.year");
+        this.day =  Integer.parseInt(numbers[0]);
+    }
+
+    public Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Date setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public Date setMonth(int month) {
+        this.month = month;
+        return this;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public Date setDay(int day) {
+        this.day = day;
+        return this;
+    }
+}
---
+++ src/main/java/pl/ferdynand/calendar/ui/model/Event.java
@@ -0,0 +1,43 @@
+package pl.ferdynand.calendar.ui.model;
+
+public class Event {
+    private String title;
+    private Date starts;
+    private Date ends;
+
+    public Event() {
+    }
+
+    public Event(String title, Date starts, Date ends) {
+        this.title = title;
+        this.starts = starts;
+        this.ends = ends;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Event setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Date getStarts() {
+        return starts;
+    }
+
+    public Event setStarts(Date starts) {
+        this.starts = starts;
+        return this;
+    }
+
+    public Date getEnds() {
+        return ends;
+    }
+
+    public Event setEnds(Date ends) {
+        this.ends = ends;
+        return this;
+    }
+}
